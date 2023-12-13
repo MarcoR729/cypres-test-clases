154973b163389d71780fd0b2f82bf2e4561e834e
Marco Ruiz <marcografico729@gmail.com>
2023-12-13 19:16:34 +0100

    para fran

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -1,7 +1,20 @@
 describe('pruebas todo', () => {
-    it('login con usuario y contraseña validos', () => {
+    it('crear tarea', () => {
       cy.visit('https://todomvc.com/examples/react/#/')
-
+      cy.get('.new-todo').type("sacar al perro{enter}")
+    
     })
-   
+    it('Marcar tarea como completada', () => {
+      cy.visit('https://todomvc.com/examples/react/#/')
+      cy.get('.new-todo').type("limpiar la casa{enter}")
+      cy.get('.toggle').click()
+    })
+
+    it('Desmarcar tarea completada', () => {
+      cy.visit('https://todomvc.com/examples/react/#/')
+      cy.get('.new-todo').type("limpiar la casa de mi abuela{enter}")
+      cy.get('.toggle').click()
+      cy.get('.toggle').click()
+    })
+
   })
